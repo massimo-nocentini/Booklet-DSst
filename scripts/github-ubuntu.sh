88ffe6193b0c66abf7b8af3f72b007c715bbb311
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-04-05 11:16:31 +0200

    Adjust the CI

--- scripts/github-ubuntu.sh
+++ scripts/github-ubuntu.sh
@@ -11,6 +11,10 @@ curl https://get.pharo.org/64/120 | bash
     repository: 'github://massimo-nocentini/Booklet-DSst/src';
     load ] on: MCMergeOrLoadWarning do: [:warning | warning load ].
 
+Smalltalk snapshot: true andQuit: true."
+
+./pharo Pharo.image eval "
+
 BookletDSstTest new 
   testBasicObjectsNotebook;
   yourself.
