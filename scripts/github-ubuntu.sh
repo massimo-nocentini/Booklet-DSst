4329ca495fa674ade6615358862428318d94dd8c
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-04-05 11:14:13 +0200

    Introduce workflow

---
+++ scripts/github-ubuntu.sh
@@ -0,0 +1,18 @@
+
+wget https://github.com/massimo-nocentini/pharo-vm/releases/latest/download/pharo-vm-ubuntu.zip
+
+unzip pharo-vm-ubuntu.zip
+
+curl https://get.pharo.org/64/120 | bash
+
+./pharo Pharo.image eval "
+[ Metacello new
+    baseline: 'BookletDSst';
+    repository: 'github://massimo-nocentini/Booklet-DSst/src';
+    load ] on: MCMergeOrLoadWarning do: [:warning | warning load ].
+
+BookletDSstTest new 
+  testBasicObjectsNotebook;
+  yourself.
+
+Smalltalk snapshot: true andQuit: true."
